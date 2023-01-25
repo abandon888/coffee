53e51873206508ee1bc47e9bf406f819c008ba89
abandon wang <1876746719@qq.com>
2023-01-25 10:23:21 +0000

    终于完成webMusicPlay,竟然是byClassName出了问题

--- webMusicPlay/index.js
+++ webMusicPlay/index.js
@@ -2,24 +2,14 @@ let play = document.getElementsByClassName('play')
 let song = document.getElementById('song')
 console.log(play,song)
 
-play.onclick =()=>{
-  console.log('yes')
+
+play[0].addEventListener('click',()=>{
+  if(song.paused){
+    song.play()
+    play[0].src='img/pause.png'
+  }else{
+    song.pause()
+    play[0].src= 'img/play.png'
+  }
 }
-// play.onclick = function(){
-//   if(play.paused){
-//     song.play()
-//     play.src='img/pause.png'
-//   }else{
-//     song.pause()
-//     play.src= 'img/play.png'
-//   }
-// }
-// play.addEventListener('click',()=>{
-//   if(play.paused){
-//     song.play()
-//     play.src='img/pause.png'
-//   }else{
-//     song.pause()
-//     play.src= 'img/play.png'
-//   }
-// })
+)
