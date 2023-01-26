ca2799295cca525c9e149692fbd847d249ffd6ed
abandon wang <1876746719@qq.com>
2023-01-26 10:12:01 +0000

    写样式写得放弃了，还是用antd吧

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -7,7 +7,41 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="text-4xl text-green-400 text-center font-semibold">yes</div>
+    <div className="border-4 container mx-auto rounded-2xl border-yellow-600 relative m-64">
+      <div className="text-4xl text-blue-400 text-center font-semibold p-32">
+        calculator
+        <input
+          type="text"
+          className="border-2 border-gray-700 rounded-2xl hover:from-gray-700 "
+        />
+        <table className="border-2 border-gray-700 rounded-2xl hover:from-gray-700 ">
+          <tr className='border-2 border-green-600'>
+            <td className='border-2 border-green-600'>1</td>
+            <td className='border-2 border-green-600'>2</td>
+            <td >3</td>
+            <td>+</td>
+          </tr>
+          <tr>
+            <td>4</td>
+            <td>5</td>
+            <td>6</td>
+            <td>-</td>
+          </tr>
+          <tr>
+            <td>7</td>
+            <td>8</td>
+            <td>9</td>
+            <td>*</td>
+          </tr>
+          <tr>
+            <td>0</td>
+            <td>.</td>
+            <td>=</td>
+            <td>/</td>
+          </tr>
+        </table>
+      </div>
+    </div>
   )
 }
 
