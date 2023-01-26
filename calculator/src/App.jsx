d2bf5978c7e7fe3e1a4a59e3beb897f66d7c128f
abandon wang <1876746719@qq.com>
2023-01-26 09:37:21 +0000

    终于引入了tailwindcss,用react+vite构建真不容易啊

---
+++ calculator/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import 'tailwindcss/tailwind.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="text-4xl text-green-400 text-center font-semibold">yes</div>
+  )
+}
+
+export default App
