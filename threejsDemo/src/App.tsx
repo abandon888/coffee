010f8383109228cb936d02be1fae6d20720dd477
abandon888 <1876746719@qq.com>
2023-04-24 11:44:32 +0800

    自建的小demo

--- threejsDemo/src/App.tsx
+++ threejsDemo/src/App.tsx
@@ -6,36 +6,21 @@ import Box from './Box.tsx'
 import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="canvas-container">
+        <Canvas>
+          <ambientLight intensity={0.1} />
+          <directionalLight color="red" position={[0, 0, 5]} />
+          <mesh>
+            <boxGeometry args={[2,2,2]}/>
+            <meshStandardMaterial color={'green'}/>
+          </mesh>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} /> <Box position={[-1.2, 0, 0]} />{' '}
+          <Box position={[1.2, 0, 0]} />{' '}
+        </Canvas>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
-      <canvas id="scene"></canvas>
-      <Canvas>
-        <ambientLight />
-        <pointLight position={[10, 10, 10]} /> <Box position={[-1.2, 0, 0]} />{' '}
-        <Box position={[1.2, 0, 0]} />{' '}
-      </Canvas>
     </>
   )
 }
