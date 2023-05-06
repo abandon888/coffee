1300e91e9e2a4ecfdf7d07efdb19ee60568f4844
abandon888 <1876746719@qq.com>
2023-05-06 09:56:33 +0800

    引入模型失败

--- threejsDemo/src/world/modelExp.tsx
+++ threejsDemo/src/world/modelExp.tsx
@@ -1,12 +1,19 @@
-import { OrbitControls } from "@react-three/drei";
-import { Perf } from "r3f-perf";
+import { OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 export default function modelExp() {
+  const model: THREE.Group = useLoader(
+    GLTFLoader,
+    '../assets/models/Flamingo.glb'
+  )
+  console.log(model)
   return (
     <>
       <OrbitControls makeDefault />
-      <directionalLight position={[0, 10, 0]} intensity={ 1.5 }/>
-      <ambientLight intensity={ 0.5 }/>
+      <directionalLight position={[0, 10, 0]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
         <planeGeometry args={[1, 1]} />
@@ -14,5 +21,5 @@ export default function modelExp() {
       </mesh>
       <Perf position="top-left" />
     </>
-  );
+  )
 }
